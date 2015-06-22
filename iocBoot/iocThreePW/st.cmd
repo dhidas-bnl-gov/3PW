36e21fb0714ce4ff54c9af13949c28f2de0533ad
Dean Andrew Hidas <dhidas@debian7.8.0>
2015-06-22 15:07:42 -0400

    playing with motor configure

--- iocBoot/iocThreePW/st.cmd
+++ iocBoot/iocThreePW/st.cmd
@@ -1,43 +1,41 @@
 #!../../bin/linux-x86_64/ThreePW
 
-## You may have to change ThreePW to something else
-## everywhere it appears in this file
-
 < envPaths
-
 cd ${TOP}
 
 
 # Set epics env variables
 epicsEnvSet("PMAC1_IP", "192.168.1.12:1025")
-
 epicsEnvSet("sys", "SR:C99-ID:G1")
 epicsEnvSet("dev", "{3PW:1}")
 epicsEnvSet ("STREAM_PROTOCOL_PATH", "${TOP}/protocols")
+
+
 
 ## Register all support components
 dbLoadDatabase("dbd/ThreePW.dbd")
 ThreePW_registerRecordDeviceDriver pdbbase
 
 
+
 # TCP/IP conection for Motor Controller
-pmacAsynIPConfigure("PMAC1port", $(PMAC_IP))
+pmacAsynIPConfigure("PMAC1port", $(PMAC1_IP))
 pmacAsynMotorCreate("PMAC1port", 0, 0, 1)
 drvAsynMotorConfigure("PMAC1", "pmacAsynCoord", 12, 1)
-pmacAsynCoordCreate("PMAC1port", 0, 2, 12, 10)
+#pmacAsynCoordCreate("PMAC1port", 0, 2, 12, 10)
 
 pmacSetIdlePollPeriod(0, 500)
 pmacSetMovingPollPeriod(0, 200)
 
 
 
-
 ## Load record instances
-#dbLoadRecords("db/pmacStatusAxis.db","SYS=$(sys),DEV=$(dev),AXIS=1,PORT=PMAC1port")
-dbLoadRecords("db/pmac_asyn_motor.db","SYS=$(sys),DEV=$(dev),AXIS=1,PORT=PMAC1port")
+#dbLoadRecords("db/pmacStatus.db","SYS=$(sys),DEV=$(dev),AXIS=1,PORT=PMAC1port")
+dbLoadRecords("db/pmacStatusAxis.db","SYS=$(sys),DEV=$(dev),AXIS=1,PORT=PMAC1port")
+#dbLoadRecords("db/pmac_asyn_motor.db","SYS=$(sys),DEV=$(dev),SPORT=PMAC1port")
 
 
 
 cd ${TOP}/iocBoot/${IOC}
 iocInit
-
+exit
