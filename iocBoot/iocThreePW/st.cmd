5a6dc9b1100b1a830329925e2903d2422aa2cc7c
Dean Andrew Hidas <dhidas@debian7.8.0>
2015-07-01 14:18:57 -0400

    minor updates

--- iocBoot/iocThreePW/st.cmd
+++ iocBoot/iocThreePW/st.cmd
@@ -5,9 +5,10 @@ cd ${TOP}
 
 
 # Set epics env variables
+epicsEnvSet("ENGINEER", "Dean Andrew Hidas is not an engineer. <dhidas@bnl.gov>")
 epicsEnvSet("PMAC1_IP", "192.168.1.12:1025")
 epicsEnvSet("sys", "SR:C99-ID:G1")
-epicsEnvSet("dev", "{3PW:1}")
+epicsEnvSet("dev", "3PW:1")
 epicsEnvSet ("STREAM_PROTOCOL_PATH", "${TOP}/protocols")
 
 
@@ -19,10 +20,14 @@ ThreePW_registerRecordDeviceDriver pdbbase
 
 
 # TCP/IP conection for Motor Controller
-pmacAsynIPConfigure("PMAC1port", $(PMAC1_IP))
-pmacAsynMotorCreate("PMAC1port", 0, 0, 1)
-drvAsynMotorConfigure("PMAC1", "pmacAsynCoord", 12, 1)
-#pmacAsynCoordCreate("PMAC1port", 0, 2, 12, 10)
+pmacAsynIPConfigure("P0", $(PMAC1_IP))
+#pmacCreateController("M0", "P0", 0, 1, 100, 1000)
+#pmacCreateAxis("M0", 1)
+#pmacSetAxisScale("M0", 1, 1)
+
+pmacAsynMotorCreate("P0", 0, 0, 1)
+#drvAsynMotorConfigure("P0", "pmacAsynCoord", 12, 1)
+#pmacAsynCoordCreate("P0", 0, 2, 12, 10)
 
 pmacSetIdlePollPeriod(0, 500)
 pmacSetMovingPollPeriod(0, 200)
@@ -30,12 +35,14 @@ pmacSetMovingPollPeriod(0, 200)
 
 
 ## Load record instances
-#dbLoadRecords("db/pmacStatus.db","SYS=$(sys),DEV=$(dev),AXIS=1,PORT=PMAC1port")
-dbLoadRecords("db/pmacStatusAxis.db","SYS=$(sys),DEV=$(dev),AXIS=1,PORT=PMAC1port")
-#dbLoadRecords("db/pmac_asyn_motor.db","SYS=$(sys),DEV=$(dev),SPORT=PMAC1port")
+#dbLoadRecords("db/motor.db","P=$(sys),M={$(dev)},MOTOR=M0,PORT=P0,ADDR=1,DESC=mymoter,DTYP=asynMotor,MRES=2e-05,EGU=mm,PREC=5")
+#dbLoadRecords("db/motorstatus.db","SYS=$(sys),DEV=$(dev),PORT=P0,AXIS=1")
+#dbLoadRecords("db/pmacStatus.db","SYS=$(sys),PMAC=$(dev),VERSION=1,PLC=5,NAXES=1,PORT=P0")
+dbLoadRecords("db/pmacStatusAxis.db","SYS=$(sys),DEV={$(dev)},AXIS=1,PORT=P0")
+#dbLoadRecords("db/pmac_asyn_motor.db","SYS=$(sys),DEV={$(dev)},SPORT=P0")
 
 
 
 cd ${TOP}/iocBoot/${IOC}
 iocInit
-exit
+#exit
